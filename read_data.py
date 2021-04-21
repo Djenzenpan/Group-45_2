ba7ea16c1aa879d22a4b0b5f31afd39451346199
Djenzenpan <jmppannekeet@gmail.com>
2021-04-21 12:14:01 +0200

    Total non-NaN and absolute total per feature in results folder

--- read_data.py
+++ read_data.py
@@ -6,20 +6,33 @@ chunksize = 10 ** 5
 
 def open_chunks(filepath, column_name):
 
+    the_dict = {}
+
     # Splits dataset into chunks and loops through them
     file = pd.read_csv(filepath, chunksize=chunksize)
-    for thing in file:
+    for i, thing in enumerate(file):
 
         # Loops over columns in current chunk
         for something in thing:
 
-            # Counts and prints number of NaNs in column and total column length
-            null_counter = thing[something].notnull().sum()
-            totalcounter = len(thing[something])
-            print(something)
-            print(totalcounter)
-            print(null_counter)
-            print("")
+            if something not in the_dict:
+                the_dict[something] = {"Not-null": 0,
+                                       "Total": 0}
+
+            else:
+                the_dict[something]["Not-null"] += thing[something].notnull().sum()
+                the_dict[something]["Total"] += len(thing[something])
+
+        print("Chunk " + str(i))
+
+    temp_string = ""
+    for feature in the_dict:
+        temp_string += str(feature) + ": " + str(the_dict[feature]) + " \n"
+
+    new_file = open("Results/counts.txt", "w")
+    new_file.write(temp_string)
+    new_file.close()
+
 
 if __name__ == "__main__":
     open_chunks("Data/train.csv", 'comp8_rate_percent_diff')
