8226a56b40aebeea56efcf752ebc728f0bb9aca6
Djenzenpan <jmppannekeet@gmail.com>
2021-04-21 11:50:57 +0200

    Read chunks, get # of NaN in column + column total

--- read_data.py
+++ read_data.py
@@ -7,17 +7,19 @@ chunksize = 10 ** 5
 def open_chunks(filepath, column_name):
 
     # Splits dataset into chunks and loops through them
-    counter = 0
-    totalcounter = 0
     file = pd.read_csv(filepath, chunksize=chunksize)
     for thing in file:
 
-        # Counts and prints number of NaNs in column and total column length
-        counter += thing[column_name].notnull().sum()
-        totalcounter += len(thing[column_name])
-        print(totalcounter)
-        print(counter)
-        print("")
+        # Loops over columns in current chunk
+        for something in thing:
+
+            # Counts and prints number of NaNs in column and total column length
+            null_counter = thing[something].notnull().sum()
+            totalcounter = len(thing[something])
+            print(something)
+            print(totalcounter)
+            print(null_counter)
+            print("")
 
 if __name__ == "__main__":
     open_chunks("Data/train.csv", 'comp8_rate_percent_diff')
