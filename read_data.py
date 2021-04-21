137c7025d8deee34f4745b9029cd8241c8bb13ca
Djenzenpan <jmppannekeet@gmail.com>
2021-04-21 12:30:29 +0200

    -

--- read_data.py
+++ read_data.py
@@ -6,29 +6,37 @@ chunksize = 10 ** 5
 
 def open_chunks(filepath, column_name):
 
-    the_dict = {}
+    # Initialize dict for counting storage
+    count_dict = {}
 
     # Splits dataset into chunks and loops through them
     file = pd.read_csv(filepath, chunksize=chunksize)
-    for i, thing in enumerate(file):
+    for i, chunk in enumerate(file):
 
         # Loops over columns in current chunk
-        for something in thing:
+        for column in chunk:
 
-            if something not in the_dict:
-                the_dict[something] = {"Not-null": 0,
+            # Adds keys to dict if not yet there
+            if column not in count_dict:
+                count_dict[column] = {"Not-null": 0,
                                        "Total": 0}
 
+            # Adds counts to proper place in dict
             else:
-                the_dict[something]["Not-null"] += thing[something].notnull().sum()
-                the_dict[something]["Total"] += len(thing[something])
+                count_dict[column]["Not-null"] += chunk[column].notnull().sum()
+                count_dict[column]["Total"] += len(chunk[column])
 
+        # Lets user know which chunk is currently being processes (total = 100)
         print("Chunk " + str(i))
 
+    # Turns dict values to string for easy writing
     temp_string = ""
-    for feature in the_dict:
-        temp_string += str(feature) + ": " + str(the_dict[feature]) + " \n"
+    for feature in count_dict:
+        temp_string += str(feature)
+        temp_string += ": Non-NaN=" + str(count_dict[feature]["Not-null"])
+        temp_string += ". Total=" + str(count_dict[feature]["Total"]) + " \n"
 
+    # Write counts string to file
     new_file = open("Results/counts.txt", "w")
     new_file.write(temp_string)
     new_file.close()
